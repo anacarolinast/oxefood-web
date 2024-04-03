b4a667e08d855bbf267ab8165783e59dbb2dcae8
Aluno <aluno@ifpe>
2024-04-03 19:01:09 -0300

    feat: function delete

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -1,11 +1,36 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import { Button, Container, Divider, Icon, Table } from "semantic-ui-react"
+import { Button, Container, Divider, Header, Icon, Modal, Table } from "semantic-ui-react"
 import MenuSistema from "../menuSistema/MenuSistema"
 
 export default function ListCliente() {
   const [lista, setLista] = useState([])
+  const [openModal, setOpenModal] = useState(false);
+  const [idRemover, setIdRemover] = useState();
+
+  function confirmaRemover(id) {
+    setOpenModal(true)
+    setIdRemover(id)
+  }
+
+  async function remover() {
+
+    await axios.delete('http://localhost:8080/api/cliente/' + idRemover)
+    .then((response) => {
+
+        console.log('Cliente removido com sucesso.')
+
+        axios.get("http://localhost:8080/api/cliente")
+        .then((response) => {
+            setLista(response.data)
+        })
+    })
+    .catch((error) => {
+        console.log('Erro ao remover um cliente.')
+    })
+    setOpenModal(false)
+}
 
   useEffect(() => {
     carregarLista()
@@ -98,6 +123,7 @@ export default function ListCliente() {
                         color='red'
                         title='Clique aqui para remover este cliente'
                         icon
+                        onClick={e => confirmaRemover(cliente.id)}
                       >
                         <Icon name='trash' />
                       </Button>
@@ -107,6 +133,25 @@ export default function ListCliente() {
               </Table.Body>
             </Table>
           </div>
+          <Modal
+            basic
+            onClose={() => setOpenModal(false)}
+            onOpen={() => setOpenModal(true)}
+            open={openModal}
+          >
+            <Header icon>
+              <Icon name='trash' />
+              <div style={{ marginTop: '5%' }}> Tem certeza que deseja remover esse registro? </div>
+            </Header>
+            <Modal.Actions>
+              <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                <Icon name='remove' /> Não
+              </Button>
+              <Button color='green' inverted onClick={() => remover()}>
+                <Icon name='checkmark' /> Sim
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </Container>
       </div>
     </div>
